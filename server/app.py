aaa1a5e9a996fa868fe784164ebfd9c16503808d
Suharsh Nagasampagi <suharsh329@gmail.com>
2020-07-19 00:51:25 +1000

    Api

--- server/app.py
+++ server/app.py
@@ -1,22 +1,22 @@
 from flask import Flask
-from flask import redirect, request, jsonify
+from flask import request, jsonify
 from flask_bcrypt import Bcrypt, check_password_hash
-from flask_login import login_user, current_user, logout_user, login_required, UserMixin
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from datetime import datetime
+from flask_login import current_user, logout_user, UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
-db = None
+
+db = SQLAlchemy()
+
 
 class Mentor(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    mentor_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
 
+
 class Student(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
@@ -24,11 +24,17 @@ class Student(db.Model, UserMixin):
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rrelepnixlppyi:80ab34ef14e38574ff6bcfc2d4e55d6a2f06c72933ffd95e9509dec552d698da@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d75dva3dnbli8b'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 bcrypt = Bcrypt(app)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route("/api/v1/register", methods=['POST'])
 def register():
@@ -52,8 +58,14 @@ def login():
         return ""
     email = request.args.get('email')
     password = request.args.get('password')
-    user = ''
-    if user is not None and check_password_hash(password, password):
+    role = request.args.get('role')
+    if role == 'student':
+        sql = f"SELECT password FROM student WHERE email = {email}"
+    else:
+        sql = f"SELECT password FROM mentor WHERE email = {email}"
+
+    user = db.session.query(sql)
+    if user is not None and check_password_hash(user.password, password):
         return jsonify({'signed_in': True})
     return jsonify({'signed_in': False})
 
